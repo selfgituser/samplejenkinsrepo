2f76440904070a58211307f84744a41db4af9ae1
pushpak <metapushguy@gmail.com>
2025-07-25 23:37:37 -0400

    Intail commit for samplemicrojenkin

---
+++ src/main/java/com/example/samplemicro/SamplemicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplemicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplemicro/SamplemicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplemicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplemicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
